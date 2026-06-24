3924fc7797cbd0b04c4719e033dc2c63e764e73f
Ryan Lintott <ryan.lintott@abetterwaytodo.com>
2026-06-24 10:20:16 -0400

    Initial Commit

---
+++ SpeechSynthesisIPAExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpeechSynthesisIPAExample
+//
+//  Created by Ryan Lintott on 2026-06-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpeechSynthesisIPAExample/SpeechSynthesisIPAExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeechSynthesisIPAExampleApp.swift
+//  SpeechSynthesisIPAExample
+//
+//  Created by Ryan Lintott on 2026-06-24.
+//
+
+import SwiftUI
+
+@main
+struct SpeechSynthesisIPAExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
